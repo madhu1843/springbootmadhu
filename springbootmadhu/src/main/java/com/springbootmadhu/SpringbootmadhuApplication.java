caafa084e7bec627d8693d1df9f42aff3f8c14cf
hp <rnaman51@gmail.com>
2023-01-27 23:14:13 +0530

    springboot madhu

---
+++ springbootmadhu/src/main/java/com/springbootmadhu/SpringbootmadhuApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmadhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmadhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmadhuApplication.class, args);
+	}
+
+}
---
+++ springbootmadhu/src/test/java/com/springbootmadhu/SpringbootmadhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmadhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmadhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
